82ad394b528a3a669eff213a0072fb7c38d7983b
Alan <alan_wz12@hotmail.com>
2023-08-26 15:44:02 -0300

    agregando funcionalidad de busqueda

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,13 +9,14 @@ android {
 
     defaultConfig {
         applicationId = "com.trabajoIntegrador.gameOfThrones"
-        minSdk = 24
+        minSdk = 28
         targetSdk = 33
         versionCode = 1
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
+
 
     buildTypes {
         release {
@@ -36,6 +37,9 @@ android {
     buildFeatures {
         viewBinding = true
     }
+    viewBinding{
+        enable=true
+    }
 }
 
 dependencies {
@@ -47,6 +51,7 @@ dependencies {
     implementation("androidx.navigation:navigation-fragment-ktx:2.7.0")
     implementation("androidx.navigation:navigation-ui-ktx:2.7.0")
     implementation ("com.github.bumptech.glide:glide:4.16.0")
+    implementation("com.android.car.ui:car-ui-lib:2.5.1")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
