ce32edd8a0eafe565498791b11a44a2bde310a56
Alan <alan_wz12@hotmail.com>
2023-09-11 16:09:54 -0300

    recycler house agregado

--- app/src/main/java/com/trabajoIntegrador/gameOfThrones/UsuarioDao.kt
+++ app/src/main/java/com/trabajoIntegrador/gameOfThrones/UsuarioDao.kt
@@ -5,6 +5,7 @@ import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.Query
 import androidx.room.Update
+import com.trabajoIntegrador.gameOfThrones.datos.Usuario
 
 @Dao
 interface UsuarioDao {
